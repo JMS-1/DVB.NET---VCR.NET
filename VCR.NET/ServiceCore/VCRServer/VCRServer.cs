73019b1c4baba49eef2a9767d72159b5f3a5cbdf
JMS-1 <dev-2@psimarron.net>
2013-12-07 14:23:00 +0100

    Interim Fix Web.Config not installer (VS2013 issue)
    Web.Config was Source AND Content, removing CONTENT doesn't install it
    in BINARY only installation mode.

--- VCR.NET/ServiceCore/VCRServer/VCRServer.cs
+++ VCR.NET/ServiceCore/VCRServer/VCRServer.cs
@@ -20,7 +20,7 @@ namespace JMS.DVBVCR.RecordingService
         /// <summary>
         /// Wird beim Bauen automatisch eingemischt.
         /// </summary>
-        private const string CURRENTDATE = "2013/12/06";
+        private const string CURRENTDATE = "2013/12/07";
 
         /// <summary>
         /// Aktuelle Version des VCR.NET Recording Service.
