7acc5b32263d56a1fa9aebb5c4c713f942d2e04a
JMS-1 <dev-2@psimarron.net>
2013-12-16 23:01:10 +0100

    New setups - something wrong with Nexus!

--- VCR.NET/ServiceCore/VCRServer/VCRServer.cs
+++ VCR.NET/ServiceCore/VCRServer/VCRServer.cs
@@ -20,7 +20,7 @@ namespace JMS.DVBVCR.RecordingService
         /// <summary>
         /// Wird beim Bauen automatisch eingemischt.
         /// </summary>
-        private const string CURRENTDATE = "2013/12/15";
+        private const string CURRENTDATE = "2013/12/16";
 
         /// <summary>
         /// Aktuelle Version des VCR.NET Recording Service.
