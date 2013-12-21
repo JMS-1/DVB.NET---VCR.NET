e923cc803616594fa59e6b93e27ebccfac728aa1
JMS-1 <dev-2@psimarron.net>
2013-12-22 00:24:48 +0100

    Still adjusting tables and links

--- VCR.NET/ServiceCore/VCRServer/VCRServer.cs
+++ VCR.NET/ServiceCore/VCRServer/VCRServer.cs
@@ -20,7 +20,7 @@ namespace JMS.DVBVCR.RecordingService
         /// <summary>
         /// Wird beim Bauen automatisch eingemischt.
         /// </summary>
-        private const string CURRENTDATE = "2013/12/21";
+        private const string CURRENTDATE = "2013/12/22";
 
         /// <summary>
         /// Aktuelle Version des VCR.NET Recording Service.
