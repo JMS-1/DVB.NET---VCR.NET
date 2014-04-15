dd28b72b75447987d1a88b2e6243c49bf388102a
JMS-1 <dev-2@psimarron.net>
2014-04-15 23:40:32 +0200

    New development area in DVB.NET (experimental) started: TV controller engine

---
+++ DVB.NET/TV Control Engine/JMS.TV.Core.UnitTests/ControllerUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace JMS.TV.Core.UnitTests
+{
+    /// <summary>
+    /// Prüft die elementaren Funktionen der Kernsteuereinheit.
+    /// </summary>
+    [TestClass]
+    public class ControllerUnitTests
+    {
+        /// <summary>
+        /// [tbd]
+        /// </summary>
+        [TestMethod]
+        public void CanCreate()
+        {
+            // Create component under test
+            var cut = TvController.Create();
+
+            // Validate
+            Assert.IsNotNull( cut, "controller" );
+        }
+    }
+}
---
+++ DVB.NET/TV Control Engine/JMS.TV.Core/TvController.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace JMS.TV.Core
+{
+    /// <summary>
+    /// Die Steuereinheit für die Fernsehanzeige.
+    /// </summary>
+    public class TvController
+    {
+        /// <summary>
+        /// Erstellt eine neue Steuereinheit.
+        /// </summary>
+        private TvController()
+        {
+        }
+
+        /// <summary>
+        /// Erstellt eine neue Steuereinheit.
+        /// </summary>
+        /// <returns>Die gewünschte Steuereinheit.</returns>
+        public static TvController Create()
+        {
+            return new TvController();
+        }
+    }
+}
