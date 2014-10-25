2a5b9f17c4626cf451f9037b9cfdd1429937d946
JMS-1 <dev-2@psimarron.net>
2014-10-25 13:25:50 +0200

    NuGet update

--- VCR.NET/ServiceCore/VCRServer/VCRServer.cs
+++ VCR.NET/ServiceCore/VCRServer/VCRServer.cs
@@ -20,7 +20,7 @@ namespace JMS.DVBVCR.RecordingService
         /// <summary>
         /// Wird beim Bauen automatisch eingemischt.
         /// </summary>
-        private const string CURRENTDATE = "2014/09/28";
+        private const string CURRENTDATE = "2014/10/25";
 
         /// <summary>
         /// Aktuelle Version des VCR.NET Recording Service.
