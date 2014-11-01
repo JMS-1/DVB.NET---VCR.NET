89866ffaff127cb54296f1b8e5e0a5228b1de497
JMS-1 <dev-2@psimarron.net>
2014-11-01 20:18:07 +0100

    Tested against WiX 3.9; Added ReBuild Command

---
+++ DVB.NET/Setups/rebuild.cmd
@@ -0,0 +1,9 @@
+set msbuild="C:\Program Files\MSBuild\12.0\Bin\MSBuild.exe"
+
+rd /s /q ..\Assemblies
+
+%msbuild% "Core\Core.sln" /p:Configuration=Release;Platform="Mixed Platforms" /v:minimal /t:Clean;Build /m /nr:false /fl /flp:Verbosity=Normal;LogFile="Core\Build.log"
+%msbuild% "Server\Server.sln" /p:Configuration=Release;Platform="Mixed Platforms" /v:minimal /t:Clean;Build /m /nr:false /fl /flp:Verbosity=Normal;LogFile="Server\Build.log"
+%msbuild% "Tools\Tools.sln" /p:Configuration=Release;Platform="Mixed Platforms" /v:minimal /t:Clean;Build /m /nr:false /fl /flp:Verbosity=Normal;LogFile="Tools\Build.log"
+
+pause
