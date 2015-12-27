29f6d7e57f98c83abb0385ca02de4e2945cc0d10
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2015-12-27 18:53:31 +0100

    Added standby tool

---
+++ VCR.NET/StandByTool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StandByTool
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault( false );
+
+            if (MessageBox.Show( new Form(), "Soll dieser Rechner in den Schlafzustand versetzt werden?", "VCR.NET Schlafzustand", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1 ) == DialogResult.Yes)
+                VCRControlCenter.VCRNETRestProxy.TryHibernate( "http://localhost/vcr.net" );
+        }
+    }
+}
+
+namespace VCRControlCenter
+{
+    static class VCRNETControl
+    {
+        public static void Log( string format, params string[] args )
+        {
+        }
+    }
+}
