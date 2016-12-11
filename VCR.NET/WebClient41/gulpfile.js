f1cc016ff9134fd6dbb7e3ee3f1ea96f0d5a0538
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2016-12-11 15:46:42 +0100

    Started to use gulp-sass

---
+++ VCR.NET/WebClient41/gulpfile.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task('sass', function () {
+    gulp.src("Content/styles.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("."));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch("Content/**/*.scss", ['sass']);
+});
