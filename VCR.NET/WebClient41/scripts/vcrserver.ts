044168a34d178e9d0fe269e8e39f2921073f2c3a
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2016-12-21 19:33:21 +0100

    Playing around with promise - enough for now!

--- VCR.NET/WebClient41/scripts/vcrserver.ts
+++ VCR.NET/WebClient41/scripts/vcrserver.ts
@@ -17,7 +17,7 @@ module VCRServer {
     // Der Präfix für alle REST Zugiffe
     var restRoot = serverRoot + '/vcr.net/';
 
-    // Hilfsklasse zur Emulation von ES6 Promises.
+    // Hilfsklasse zur Emulation von ES6 Promises - die Implementierung ist weit von korrekt (in verschiedenen Aspekten), für uns reicht es aber so.
     class PromiseHelper<TResponseType> implements Thenable<TResponseType>{
         // Wird im Erfolgsfall aufgerufen.
         private _success: (value: TResponseType) => any | Thenable<any>;
@@ -29,7 +29,7 @@ module VCRServer {
         private _next: PromiseHelper<any>[] = [];
 
         // Meldet die Ergebnisauswertung an.
-        then<TProjectedType>(onFulfilled?: (value: TResponseType) => TProjectedType, onRejected?: (error: any) => TProjectedType): Thenable<TProjectedType> {
+        then<TProjectedType>(onFulfilled?: (value: TResponseType) => TProjectedType | Thenable<TProjectedType>, onRejected?: (error: any) => TProjectedType | Thenable<TProjectedType>): Thenable<TProjectedType> {
             this._success = onFulfilled;
             this._failure = onRejected;
 
@@ -41,18 +41,41 @@ module VCRServer {
             return next;
         }
 
+        // Prüft, ob eine Weiterleitung verwendet werden soll.
+        private propagate(test: any): boolean {
+            if (!test)
+                return false;
+
+            // Macht nur Sinn wenn das Objekt eine then Methode hat - hm, geht das nicht auch sicherer?
+            var thenMethod = test["then"];
+            if (!thenMethod)
+                return false;
+            if (!(thenMethod instanceof Function))
+                return false;
+
+            // Irgendwann später probieren wir es noch einmal.
+            var thenable: Thenable<any> = test;
+
+            // Wenn der asnychrone Zugriff denn dann durch ist, dann können wir das durchreichen.
+            thenable.then(value => this._next.forEach(next => next.success(value)), error => this._next.forEach(next => next.failure(next)));
+
+            return true;
+        }
+
         // Daten melden.
         success(value: TResponseType): void {
             var processedValue = this._success ? this._success(value) : value;
 
-            this._next.forEach(next => next.success(processedValue));
+            if (!this.propagate(processedValue))
+                this._next.forEach(next => next.success(processedValue));
         }
 
         // Fehler melden.
         failure(error: any): void {
             var processedError = this._failure ? this._failure(error) : error;
 
-            this._next.forEach(next => next.failure(processedError));
+            if (!this.propagate(processedError))
+                this._next.forEach(next => next.failure(processedError));
         }
     }
 
