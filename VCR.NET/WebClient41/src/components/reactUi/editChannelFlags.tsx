478c8ef807b3d70c7c3a47c5d6a3e3b41a837d00
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-01-01 15:41:48 +0100

    Minor UI tweaks

--- VCR.NET/WebClient41/src/components/reactUi/editChannelFlags.tsx
+++ VCR.NET/WebClient41/src/components/reactUi/editChannelFlags.tsx
@@ -4,10 +4,14 @@ namespace VCRNETClient.Ui {
 
     // Schnittstelle zur Pflege der in eine Aufzeichnung zu integrierenden Sonderdaten.
     interface IEditChannelFlagsStatic {
+        // Zugriff auf die einzelnen Wahrheitswerte.
         noui: App.NoUi.ISourceFlagsEditor;
     }
 
+    // React.Js Komponente zur visuellen Darstellung der Aufzeichnungsoptionen.
     export class EditChannelFlags extends React.Component<IEditChannelFlagsStatic, INoDynamicState>  {
+
+        // Erzeugt die visuelle Darstellung.
         render(): JSX.Element {
             return <div className="vcrnet-editchannelflags">
                 <EditBoolean noui={this.props.noui.includeDolby} />
@@ -16,5 +20,6 @@ namespace VCRNETClient.Ui {
                 <EditBoolean noui={this.props.noui.withSubtitles} />
             </div>;
         }
+
     }
 }
