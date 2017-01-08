925d282ea725df18f03f196e3e5298c1595b4ac3
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-01-08 17:26:30 +0100

    Links per app route

--- VCR.NET/WebClient41/src/pages/home.tsx
+++ VCR.NET/WebClient41/src/pages/home.tsx
@@ -9,10 +9,10 @@ namespace VCRNETClient {
                 <div>
                     Willkommen zur Benutzeroberfläche des VCR.NET Recording Service. Von hier aus geht es direkt zu:
                     <ul>
-                        <li><InternalLink pict="plan" view="plan">dem Aufzeichnungsplan</InternalLink> mit den anstehenden Aufzeichnungen<HelpLink page={this.props.noui} topic="parallelrecording" /></li>
+                        <li><InternalLink pict="plan" view={this.props.noui.application.planPage.route}>dem Aufzeichnungsplan</InternalLink> mit den anstehenden Aufzeichnungen<HelpLink page={this.props.noui} topic="parallelrecording" /></li>
                         <li><InternalLink pict="devices" view="current">den laufenden Aufzeichnungen</InternalLink> mit den Aktivitäten der einzelnen DVB Geräte</li>
                         <li><InternalLink pict="guide" view="guide">der Programmzeitschrift</InternalLink> zum Anlegen neuer Aufzeichnungen<HelpLink page={this.props.noui} topic="epg" /></li>
-                        <li><InternalLink pict="new" view="edit">einer neuen Aufzeichnung</InternalLink></li>
+                        <li><InternalLink pict="new" view={this.props.noui.application.editPage.route}>einer neuen Aufzeichnung</InternalLink></li>
                     </ul>
                     <ul>
                         <li><InternalLink pict="jobs" view="jobs">den vorhandenen Aufzeichnungen</InternalLink>, um diese zu verändern oder ins Archiv zu übertragen</li>
@@ -33,7 +33,7 @@ namespace VCRNETClient {
                     </ul>
                 </div>
                 <div>
-                    Weitere Informationen zum VCR.NET Recording Service findet man hier im Bereich der <InternalLink view="faq;overview">Fragen &amp; Antworten</InternalLink>, auf
+                    Weitere Informationen zum VCR.NET Recording Service findet man hier im Bereich der <InternalLink view={`${this.props.noui.application.helpPage.route};overview`}>Fragen &amp; Antworten</InternalLink>, auf
                     der <ExternalLink url="http://www.psimarron.net/vcrnet">Homepage im Internet</ExternalLink> oder
                     im <ExternalLink url="http://www.watchersnet.de/Default.aspx?tabid=52&g=topics&f=17">offiziellen Forum</ExternalLink>.
                 </div>
