90e519c00ccc7976040e3ed630877caab27b941a
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-02-10 22:53:51 +0100

    Other settings added to interface

--- VCR.NET/WebClient41/src/app/pages/admin/other.ts
+++ VCR.NET/WebClient41/src/app/pages/admin/other.ts
@@ -2,10 +2,69 @@
 
 namespace VCRNETClient.App.Admin {
 
+    export enum HibernationMode {
+        disabled,
+
+        standBy,
+
+        hibernate
+    }
+
     export interface IAdminOtherPage extends IAdminSection {
+        readonly port: JMSLib.App.IValidatedNumber;
+
+        readonly ssl: JMSLib.App.IValidatedFlag;
+
+        readonly securePort: JMSLib.App.IValidatedNumber;
+
+        readonly basicAuth: JMSLib.App.IValidatedFlag;
+
+        readonly preSleep: JMSLib.App.IValidatedNumber;
+
+        readonly minSleep: JMSLib.App.IValidatedNumber;
+
+        readonly ignoreMinSleep: JMSLib.App.IValidatedFlag;
+
+        readonly logKeep: JMSLib.App.IValidatedNumber;
+
+        readonly jobKeep: JMSLib.App.IValidatedNumber;
+
+        readonly noH264PCR: JMSLib.App.IValidatedFlag;
+
+        readonly noMPEG2PCR: JMSLib.App.IValidatedFlag;
+
+        readonly hibernation: JMSLib.App.IValueFromList<HibernationMode>;
+
+        readonly logging: JMSLib.App.IValueFromList<string>;
     }
 
     export class OtherSection extends AdminSection<VCRServer.OtherSettingsContract> implements IAdminOtherPage {
+
+        readonly port = new JMSLib.App.EditNumber({}, "webPort", () => this.refreshUi(), "TCP/IP Port für den Web Server", true, 1, 65635);
+
+        readonly ssl = new JMSLib.App.EditFlag({}, "ssl", null, "Sichere Verbindung zusätzlich anbieten");
+
+        readonly securePort = new JMSLib.App.EditNumber({}, "sslPort", () => this.refreshUi(), "TCP/IP Port für den sicheren Zugang", true, 1, 65635);
+
+        readonly basicAuth = new JMSLib.App.EditFlag({}, "basicAuth", null, "Benutzererkennung über Basic (RFC 2617) zusätzlich erlauben (nicht empfohlen)");
+
+        readonly preSleep = new JMSLib.App.EditNumber({}, "hibernationDelay", () => this.refreshUi(), "Vorlaufzeit für das Aufwachen aus dem Schlafzustand in Sekunden", true, 0, 600);
+
+        readonly minSleep = new JMSLib.App.EditNumber({}, "forcedHibernationDelay", () => this.refreshUi(), "Minimale Pause nach einem erzwungenen Schlafzustand in Minuten", true, 5, 60);
+
+        readonly ignoreMinSleep = new JMSLib.App.EditFlag({}, "suppressHibernationDelay", null, "Pause für erzwungenen Schlafzustand ignorieren");
+
+        readonly logKeep = new JMSLib.App.EditNumber({}, "protocol", () => this.refreshUi(), "Aufbewahrungsdauer für Protokolle in Wochen", true, 1, 13);
+
+        readonly jobKeep = new JMSLib.App.EditNumber({}, "archive", () => this.refreshUi(), "Aufbewahrungsdauer von archivierten Aufzeichnungen in Wochen", true, 1, 13);
+
+        readonly noH264PCR = new JMSLib.App.EditFlag({}, "noH264PCR", null, "Systemzeit (PCR) in Aufzeichnungsdateien nicht aus einem H.264 Bildsignal ableiten");
+
+        readonly noMPEG2PCR = new JMSLib.App.EditFlag({}, "noMPEG2PCR", null, "Systemzeit (PCR) in Aufzeichnungsdateien nicht aus einem MPEG2 Bildsignal ableiten");
+
+        readonly hibernation = new JMSLib.App.EditFromList<HibernationMode>({ value: null }, "value", null, "Art des von VCR.NET ausgelösten Schlafzustands", false, []);
+
+        readonly logging = new JMSLib.App.EditFromList<string>({}, "logging", null, "Umfang der Protokollierung in das Windows Ereignisprotokoll", false, []);
 
         reset(): void {
             this.update.message = ``;
@@ -13,18 +72,31 @@ namespace VCRNETClient.App.Admin {
             VCRServer.getOtherSettings().then(settings => this.setSettings(settings));
         }
 
-        private _other: VCRServer.OtherSettingsContract;
-
         private setSettings(settings: VCRServer.OtherSettingsContract): void {
-            this._other = settings;
+            this.ignoreMinSleep.data = settings;
+            this.noMPEG2PCR.data = settings;
+            this.securePort.data = settings;
+            this.basicAuth.data = settings;
+            this.noH264PCR.data = settings;
+            this.minSleep.data = settings;
+            this.preSleep.data = settings;
+            this.jobKeep.data = settings;
+            this.logKeep.data = settings;
+            this.logging.data = settings;
+            this.port.data = settings;
+            this.ssl.data = settings;
 
             this.page.application.isBusy = false;
         }
 
         protected readonly saveCaption = "Ändern und eventuell neu Starten";
 
+        protected get canSave(): boolean {
+            return false;
+        }
+
         protected saveAsync(): JMSLib.App.IHttpPromise<boolean> {
-            return VCRServer.setOtherSettings(this._other);
+            return VCRServer.setOtherSettings(this.port.data);
         }
     }
 }
--- VCR.NET/WebClient41/src/pages/admin/other.tsx
+++ VCR.NET/WebClient41/src/pages/admin/other.tsx
@@ -6,7 +6,48 @@ namespace VCRNETClient.Ui {
         render(): JSX.Element {
             return <div className="vcrnet-admin-other">
                 <h2>Sonstige Betriebsparameter</h2>
-                <JMSLib.ReactUi.ButtonCommand noui={this.props.noui.update} />
+                <form>
+                    <Field label={`${this.props.noui.port.text}:`} page={this.props.noui.page} >
+                        <JMSLib.ReactUi.EditNumber noui={this.props.noui.port} chars={8} />
+                    </Field>
+                    <div>
+                        <JMSLib.ReactUi.EditBoolean noui={this.props.noui.ssl} />
+                    </div>
+                    <Field label={`${this.props.noui.securePort.text}:`} page={this.props.noui.page} >
+                        <JMSLib.ReactUi.EditNumber noui={this.props.noui.securePort} chars={8} />
+                    </Field>
+                    <div>
+                        <JMSLib.ReactUi.EditBoolean noui={this.props.noui.basicAuth} />
+                    </div>
+                    <Field label={`${this.props.noui.hibernation.text}:`} page={this.props.noui.page} >
+                        <JMSLib.ReactUi.SelectSingleFromList noui={this.props.noui.hibernation} />
+                    </Field>
+                    <Field label={`${this.props.noui.preSleep.text}:`} page={this.props.noui.page} >
+                        <JMSLib.ReactUi.EditNumber noui={this.props.noui.preSleep} chars={8} />
+                    </Field>
+                    <Field label={`${this.props.noui.minSleep.text}:`} page={this.props.noui.page} >
+                        <JMSLib.ReactUi.EditNumber noui={this.props.noui.minSleep} chars={8} />
+                    </Field>
+                    <JMSLib.ReactUi.EditBoolean noui={this.props.noui.ignoreMinSleep} />
+                    <Field label={`${this.props.noui.logKeep.text}:`} page={this.props.noui.page} >
+                        <JMSLib.ReactUi.EditNumber noui={this.props.noui.logKeep} chars={8} />
+                    </Field>
+                    <Field label={`${this.props.noui.jobKeep.text}:`} page={this.props.noui.page} >
+                        <JMSLib.ReactUi.EditNumber noui={this.props.noui.jobKeep} chars={8} />
+                    </Field>
+                    <div>
+                        <JMSLib.ReactUi.EditBoolean noui={this.props.noui.noH264PCR} />
+                    </div>
+                    <div>
+                        <JMSLib.ReactUi.EditBoolean noui={this.props.noui.noMPEG2PCR} />
+                    </div>
+                    <Field label={`${this.props.noui.logging.text}:`} page={this.props.noui.page} >
+                        <JMSLib.ReactUi.SelectSingleFromList noui={this.props.noui.logging} />
+                    </Field>
+                </form>
+                <div>
+                    <JMSLib.ReactUi.ButtonCommand noui={this.props.noui.update} />
+                </div>
             </div>;
         }
     }
