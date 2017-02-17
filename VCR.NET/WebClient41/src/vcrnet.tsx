914498907c0347d47f6175e77ab15f7e3cf6f6e9
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-02-17 21:20:51 +0100

    Moved content files to such folder

--- VCR.NET/WebClient41/src/vcrnet.tsx
+++ VCR.NET/WebClient41/src/vcrnet.tsx
@@ -2,7 +2,7 @@
     // Initialisiert die React.js Laufzeitumgebung.
     export function startup(): void {
         // Bilbliothek konfigurieren.
-        JMSLib.ReactUi.Pictogram.imageRoot = `ui/images/`;
+        JMSLib.ReactUi.Pictogram.imageRoot = `content/images/`;
 
         // Anwendung starten.
         ReactDOM.render(<Ui.Main />, document.querySelector(`vcrnet-spa`));
