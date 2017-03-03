1c8cd7b0eb340c3dfeafc4be2263e5786702b2be
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-03-03 10:57:55 +0100

    Styling timebar

--- VCR.NET/WebClient41/src/lib.react/timeBar.tsx
+++ VCR.NET/WebClient41/src/lib.react/timeBar.tsx
@@ -7,9 +7,9 @@
         render(): JSX.Element {
             return <div className="jmslib-timebar">
                 <div>
-                    <div style={{ width: `${this.props.uvm.prefixTime}%` }}></div>
+                    {(this.props.uvm.prefixTime > 0) && <div style={{ width: `${this.props.uvm.prefixTime}%` }}></div>}
                     <div style={{ width: `${this.props.uvm.time}%`, left: `${this.props.uvm.prefixTime}%` }} className={this.props.uvm.timeIsComplete ? `jmslib-timebar-good` : `jmslib-timebar-bad`}></div>
-                    <div style={{ width: `${this.props.uvm.suffixTime}%`, left: `${this.props.uvm.prefixTime + this.props.uvm.time}%` }}></div>
+                    {(this.props.uvm.suffixTime > 0) && <div style={{ width: `${this.props.uvm.suffixTime}%`, left: `${this.props.uvm.prefixTime + this.props.uvm.time}%` }}></div>}
                 </div>
                 <div style={(this.props.uvm.currentTime === undefined) ? { display: `none` } : { left: `${this.props.uvm.currentTime}%` }}></div>
             </div>;
