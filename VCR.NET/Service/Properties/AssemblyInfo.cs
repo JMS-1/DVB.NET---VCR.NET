78fd1ae69f9cb3d39d08a324421573c50e2282f3
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-03-05 11:44:38 +0100

    Version stamp (htis time: VCR.NET only, DVB.NET stays at 4.3)

--- VCR.NET/Service/Properties/AssemblyInfo.cs
+++ VCR.NET/Service/Properties/AssemblyInfo.cs
@@ -26,4 +26,6 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion( "4.3.0.0" )]
+[assembly: AssemblyVersion( "4.5.0.0" )]
+[assembly: AssemblyFileVersion("4.5.0.0")]
+
--- VCR.NET/ServiceCore/Properties/AssemblyInfo.cs
+++ VCR.NET/ServiceCore/Properties/AssemblyInfo.cs
@@ -34,7 +34,8 @@ using JMS.DVBVCR.RecordingService.WebServer;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "4.3.0.0" )]
+[assembly: AssemblyVersion( "4.5.0.0" )]
+[assembly: AssemblyFileVersion("4.5.0.0")]
 
 // Startcode für die Web Anwendung
 [assembly: PreApplicationStartMethod( typeof( ServerRuntime ), "WebStartup" )]
