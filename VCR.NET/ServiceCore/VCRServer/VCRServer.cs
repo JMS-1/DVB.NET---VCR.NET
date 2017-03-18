2a15faf9f96a6873db8903e3b030e2eddf50acfe
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-03-18 09:19:17 +0100

    Beautifications help

--- VCR.NET/ServiceCore/VCRServer/VCRServer.cs
+++ VCR.NET/ServiceCore/VCRServer/VCRServer.cs
@@ -20,7 +20,7 @@ namespace JMS.DVBVCR.RecordingService
         /// <summary>
         /// Wird beim Bauen automatisch eingemischt.
         /// </summary>
-        private const string CURRENTDATE = "2017/03/17";
+        private const string CURRENTDATE = "2017/03/18";
 
         /// <summary>
         /// Aktuelle Version des VCR.NET Recording Service.
