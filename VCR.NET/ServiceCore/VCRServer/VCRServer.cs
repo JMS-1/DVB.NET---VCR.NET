fbbef40e7ed37fd2812668f44d8aa8d4cdceaa17
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2019-11-10 21:19:02 +0100

    prepare to work with node 12 / gulp 4 / ts 3.7.2

--- VCR.NET/ServiceCore/VCRServer/VCRServer.cs
+++ VCR.NET/ServiceCore/VCRServer/VCRServer.cs
@@ -20,7 +20,7 @@ namespace JMS.DVBVCR.RecordingService
         /// <summary>
         /// Wird beim Bauen automatisch eingemischt.
         /// </summary>
-        private const string CURRENTDATE = "2017/06/03";
+        private const string CURRENTDATE = "2019/11/10";
 
         /// <summary>
         /// Aktuelle Version des VCR.NET Recording Service.
--- VCR.NET/WebClient41/gulpfile.js
+++ VCR.NET/WebClient41/gulpfile.js
@@ -3,12 +3,12 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 
-gulp.task('sass', function () {
+gulp.task('sass', () =>
   gulp.src("Content/styles.scss")
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest("Content"));
-});
+      .pipe(gulp.dest("Content"))
+);
 
-gulp.task('sass:watch', function () {
-  gulp.watch(["content/**/*.scss", "src/**/*.scss"], ['sass']);
-});
+gulp.task('sass:watch', () => 
+  gulp.watch(["content/**/*.scss", "src/**/*.scss"], gulp.series('sass'))
+);
