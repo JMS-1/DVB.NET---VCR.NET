b49d4282f52f69d7471d972a929445880669872c
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2019-11-17 10:46:10 +0100

    remove file from setup

--- VCR.NET/ServiceCore/VCRServer/VCRServer.cs
+++ VCR.NET/ServiceCore/VCRServer/VCRServer.cs
@@ -20,7 +20,7 @@ namespace JMS.DVBVCR.RecordingService
         /// <summary>
         /// Wird beim Bauen automatisch eingemischt.
         /// </summary>
-        private const string CURRENTDATE = "2019/11/16";
+        private const string CURRENTDATE = "2019/11/17";
 
         /// <summary>
         /// Aktuelle Version des VCR.NET Recording Service.
